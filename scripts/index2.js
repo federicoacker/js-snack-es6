bee3bb74963b6bdc4763848139ad22bf541b2825
federicoacker <acker.federico@gmail.com>
2026-04-09 14:50:49 +0200

    Messi commenti e creata la funzione che estrae dall'array di squadre, un altro array contenente oggetti fatti solo con nomi e falli subiti come proprietà

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,5 +1,6 @@
-const MAX_POINTS = 30;
-const MAX_FOULS = 10;
+const MAX_POINTS = 30; // Il massimo di punti che voglio ogni squadra possa avere
+const MAX_FOULS = 10; // Il massimo di falli subiti che voglio ogni squadra possa avere
+//Array con tutte le squadre
 const teamArray = [
     {
         name:"Atalanta",
@@ -103,12 +104,38 @@ const teamArray = [
     }
 ];
 
+//FUNZIONE che popola i valori "scoredPoints" e "foulsDrawn" delle singole squadre
 function populateTeams(){
-    for(let i = 0; i < teamArray.length; i++){
+    for(let i = 0; i < teamArray.length; i++){ //Semplicemente itera nell'array di squadre
         const randomPoints = Math.floor(Math.random() * (MAX_POINTS + 1)); //Genera un numero a caso tra 0 e 30 (floor di un random * 31 sarà da 0 a 30)
-        const randomFouls = Math.floor(Math.random() * (MAX_FOULS + 1)); // Ggenera un numero a caso tra 0 e 10
-        const currentTeam = teamArray[i];
-        currentTeam.scoredPoints = randomPoints;
-        currentTeam.foulsDrawn = randomFouls;
+        const randomFouls = Math.floor(Math.random() * (MAX_FOULS + 1)); // Genera un numero a caso tra 0 e 10
+        const currentTeam = teamArray[i]; // Seleziona la squadra corrente
+        currentTeam.scoredPoints = randomPoints; //Aggiorna i suo punti con il valore generato prima
+        currentTeam.foulsDrawn = randomFouls; //Aggiorna i suoi falli subiti con il valore generato prima
     }
 }
+
+//Funzione che estrae un array di oggetti contenenti solo nome e fallo subito dall'array iniziale
+function extractFoulArray(teamsArray){
+    if(!Array.isArray(teamsArray)){ // Restituiamo codice d'errore se quello che ci viene passato non è un array
+        return -1;
+    }
+
+    const foulArray = []; // Creiamo l'array che poi restituiremo
+    for(let i = 0; i < teamsArray.length; i++){ //Iteriamo nell'array che ci è stato passato in funzione
+        const currentTeam = teamsArray[i]; // Selezioniamo la squadra individuata in questo ciclo
+
+        const tempTeam = { // Creiamo una squadra temporanea, conterrà solo nome e falli subiti della squadra corrente
+            name: currentTeam.name,
+            foulsDrawn: currentTeam.foulsDrawn
+        };
+
+        foulArray.push(tempTeam); // La mettiamo nell'array che vogliamo restituire
+    }
+
+    return foulArray; // Restituiamo l'array
+}
+
+populateTeams(); // Generiamo i numeri per tutte le squadre
+const fouledArray = extractFoulArray(teamArray); // Estraiamo l'array con solo nomi e falli subiti
+console.log(fouledArray); // logghiamo l'array.
