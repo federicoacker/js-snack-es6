7cbb96569c3a02d4f38c6c07561240bcc1068c7a
federicoacker <acker.federico@gmail.com>
2026-04-09 16:56:27 +0200

    Aggiunto funzione extra che crea una stringa formattata con tutte le bici che avevano lo stesso valore minimo di peso. Cambiato anche modo in cui vengono nominate le bici in modo che possa differenziare tra due bici con nome uguale. Aggiunto ciclo extra alla funzione findLightest che dopo aver trovato il peso più leggero, controlla nell'array se ci sono altre bici con quel peso e le aggiunge ad un array che viene poi dato alla funzione che crea la stringa di risultato

--- scripts/index1.js
+++ scripts/index1.js
@@ -23,7 +23,7 @@ function createObjectArray(arraySize){ //Accetta una arraySize a scelta dell'ute
         const randomWeight = (Math.floor(Math.random() * (20 - 5)) + 1) + 5; // Peso random da 6 a 20 (che saranno kg);
         const tempBike =  // Creiamo la bicicletta dandogli nome e peso
         {
-            name: names[randomNameIndex], 
+            name: `${names[randomNameIndex]} Mk.${i+1}`, 
             weight: `${randomWeight}kg`
         };
         objectArray.push(tempBike); // Pushiamola nell'array
@@ -39,6 +39,8 @@ function findLightest(bikeArray){
     }
     let lightest = bikeArray[0]; //Inizializziamo la bici più leggera come la prima
 
+    const lightestArray = [];
+    
     for(let i = 0; i < bikeArray.length; i++){//Iteriamo tutte le bici
         const currentBike = bikeArray[i]; // Selezioniamo la bici corrente
         const currentBikeWeight = parseInt(currentBike.weight); // Selezioniamo il suo peso, dovrebbe parsarmi solo il peso, togliendo il kg alla fine
@@ -46,8 +48,66 @@ function findLightest(bikeArray){
             lightest = bikeArray[i]; // Allora aggiorniamo la variabile lightest dandogli il riferimento alla nuova bici
         }
     }
+    // Vediamo ora se ci sono più bici che hanno tutte lo stesso peso di lightest
 
-    return lightest; // Restituiamo la lightest, che è il riferimento alla bici con weight più basso
+    for(let i = 0; i < bikeArray.length; i++){ // Iteriamo l'array
+        const currentBike = bikeArray[i]; // Selezioniamo la bici corrente
+        const currentBikeWeight = parseInt(currentBike.weight); // Selezioniamo il suo peso, dovrebbe parsarmi solo il peso, togliendo il kg alla fine
+        if(currentBikeWeight === parseInt(lightest.weight)){ // Se il suo peso è inferiore a quello della bici lightest
+            lightestArray.push(currentBike);
+        }
+    }
+
+    const returnString = createBikeString(lightestArray); // Andiamo a comporre la stringa che conterrà tutte le bici che hanno lo stesso peso di lightest
+
+    return returnString; //Restituisci la stringa
+}
+
+// FUNZIONE che crea una stringa formattata per il display del risultato in console
+function createBikeString(lightestBikeArray){
+    if(!Array.isArray(lightestBikeArray)){ // Innanzitutto controlla di ricevere un'array e se non lo è, ti da codice d'errore
+        return -1;
+    }
+
+    let tempString = ""; // Inizializziamo una tempString vuota
+    
+    if(lightestBikeArray.length === 1){
+        if(!lightestBikeArray[0].name && !lightestBikeArray[0].weight){// Se non ha la property name o weight, ritorna errore
+            return -1;
+        }
+
+        // Altrimenti componi la stringa per 1 bicicletta, quella che è nell'array
+        tempString = `La bici più leggera è la bellissima:
+
+        ${lightestBikeArray[0].name} 
+        - pesa solo:
+        ${lightestBikeArray[0].weight}
+        `;
+    }
+    else{
+        
+        // Comincia a comporre la stringa per più bici
+        tempString = `Le bici più leggere sono le bellissime:
+
+        `;
+        for(let i = 0; i < lightestBikeArray.length; i++){
+            if(!lightestBikeArray[i].name && !lightestBikeArray[i].weight){ // Fai sempre il controllo ad ogni entry dell'array, che abbia le properties name e weight
+                return -1;
+            }
+
+            tempString+=`${lightestBikeArray[i].name}`; //Aggiungi alla stringa il nome della bici
+            if(i<lightestBikeArray.length - 1){ // Se non siamo alla fine dell'array, aggiungi anche le virgole + spazio alla stringa, come separatori dei nomi delle bici
+                tempString+=`, `;
+            }
+        }
+        // Infine fuori dal loop, aggiungi anche quanto pesano, tanto hanno tutte lo stesso peso
+        tempString += `
+        - pesano solo, tutte:
+        ${lightestBikeArray[0].weight}
+        `; 
+    }
+
+    return tempString; // Restituiscimi la stringa formattata bene
 }
 
 const bikeArray = createObjectArray(20); // Creiamo il nostro array di bici
@@ -60,10 +120,7 @@ if(bikeArray === -1 || lightest === -1){
         Controlla il codice`
     );
 }
-else{
-    console.log(`La bici più leggera è la bellissima: 
-        ${lightest.name} 
-        - pesa solo:
-        ${lightest.weight}`
-    ); //Stampiamo il risultato
+else{ // Altrimenti stampa il risultato
+    console.log(lightest);
 }
+
