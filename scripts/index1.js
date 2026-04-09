db5174fad71f1f07297a88cad7bcdc19083affe9
federicoacker <acker.federico@gmail.com>
2026-04-09 14:10:15 +0200

    Creato un array con vari nomi possibili per le bici, iniziato a creare una funzione che mi crei dinamicamente un array di oggetti bicicletta

---
+++ scripts/index1.js
@@ -0,0 +1,21 @@
+const names = [
+    "Pedala e Piangi", 
+    "Ciclofallo", 
+    "Bici da Brutta Figura", 
+    "Sella Spezzaossa", 
+    "GraffiaGomiti", 
+    "Catena del Peccato", 
+    "Forcella del Disastro",
+    "Bici del Mai Più",
+    "Pedalata Mortale",
+    "Due Ruote di Vergogna"
+]
+
+function createObjectArray(arraySize){
+    if(arraySize <= 0){
+        return -1;
+    }
+
+    const objectArray = [];
+    for(let i = 0; i<arraySize; i++)
+}
